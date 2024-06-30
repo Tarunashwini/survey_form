88af3ec0c09f226607f49e12921c82a1f25abd54
Tarun_Ashwini <tarunaswini23@gmail.com>
2024-06-30 21:36:41 +0530

    created uploadfiles and Dockerfile

---
+++ application/uploadfiles.py
@@ -0,0 +1,29 @@
+from azure.storage.blob import BlobServiceClient
+
+print("azure")
+
+# Your storage account credentials
+storage_account_key = "xX5KlLkFtHNvBcHv8DKSANjt83SUAchjl3DcJu/Xgm1ShOFqXE4PPZ0jjAE1ImdqsEJWI3hSfsFe+AStekyXaA=="
+storage_account_name = "storageforstreamlit"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=storageforstreamlit;AccountKey=xX5KlLkFtHNvBcHv8DKSANjt83SUAchjl3DcJu/Xgm1ShOFqXE4PPZ0jjAE1ImdqsEJWI3hSfsFe+AStekyXaA==;EndpointSuffix=core.windows.net"
+container_name = "streamlitstoragecontainer"
+
+def upload_to_blob_storage(file_path, file_name):
+    try:
+        # Create a BlobServiceClient using the connection string
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        # Get a BlobClient to interact with the specified blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+
+        # Open the file and upload its contents to Azure Blob Storage
+        with open(file_path, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+        
+        print("Uploaded " + file_name + " file successfully.")
+    
+    except Exception as e:
+        print(f"Failed to upload {file_name} due to {str(e)}")
+q
+# Example usage
+# upload_to_blob_storage("path/to/your/file.txt", "uploaded_file_name.txt")
